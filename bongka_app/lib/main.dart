5fe88490afd64ae7f6c1807aaaf66b5acfc3c377
PanhaCPU <rysopanha234@gmail.com>
2026-07-07 18:13:15 +0700

    fix: resolve merge conflict in main.dart

--- bongka_app/lib/main.dart
+++ bongka_app/lib/main.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'theme/app_theme.dart';
-import 'ui/screen/auth/login_screen.dart';
 import 'provider/auth_provider.dart';
 import 'provider/scan_provider.dart';
 import 'provider/theme_provider.dart';
+import 'ui/screen/auth/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-=======
-
-import 'theme/app_theme.dart';
-import 'ui/screen/auth/login_screen.dart';
-
-void main() {
->>>>>>> 155565cc5fe36546ef9c6118ccee665d19a5ba0f
   runApp(const MyApp());
 }
 
@@ -27,7 +19,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
@@ -44,13 +35,6 @@ class MyApp extends StatelessWidget {
           );
         },
       ),
-=======
-    return MaterialApp(
-      title: 'Bongka',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const LoginScreen(),
->>>>>>> 155565cc5fe36546ef9c6118ccee665d19a5ba0f
     );
   }
 }
